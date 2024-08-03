ce2872fb508cfaa39be1653069041d06b5caa4ca
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-08-03 23:09:42 +0530

    Update worker.service.ts

--- angular-frontend/src/app/services/worker.service.ts
+++ angular-frontend/src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://13.201.130.220:8080/api/v1/workers";
+  private getUrl: string = "http://backend-service:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
